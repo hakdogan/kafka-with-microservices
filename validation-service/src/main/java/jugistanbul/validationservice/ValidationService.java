ca634c758b4d4574a4f90993bd0e5be0f64417ea
Hüseyin Akdoğan <hakdogan75@gmail.com>
2020-08-19 17:22:00 +0300

    Validation service talks to Kafka instead of Order service

--- validation-service/src/main/java/jugistanbul/validationservice/ValidationService.java
+++ validation-service/src/main/java/jugistanbul/validationservice/ValidationService.java
@@ -1,13 +1,85 @@
 package jugistanbul.validationservice;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jugistanbul.entity.EventObject;
+import jugistanbul.validationservice.kafka.consumer.StockCheckEventConsumer;
+import jugistanbul.validationservice.kafka.producer.ValidationEventProducer;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.errors.ProducerFencedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author hakdogan (hakdogan@kodcu.com)
  * Created on 17.08.2020
  **/
-@ApplicationPath("api")
-public class ValidationService extends Application
-{
+
+public class ValidationService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidationService.class);
+    private static final Consumer<Integer, EventObject> consumer = StockCheckEventConsumer.build();
+    private static final Producer<Integer, EventObject> producer = ValidationEventProducer.build();
+
+    public static void main(String[] args) {
+
+        ConsumerRecords<Integer, EventObject> records;
+        try {
+            EventObject event;
+            while (true) {
+                records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<Integer, EventObject> record : records) {
+                    LOGGER.info("Consumed record in validation service method: Key {} Value {} " +
+                                    "Partition {} Offset {}", record.key(), record.value(),
+                            record.partition(), record.offset());
+
+                    event = record.value();
+                    event.setNumberValid(isCardNumberValid(event.getCardNumber()));
+                    publishValidationEvent(event);
+                }
+                consumer.commitSync();
+            }
+        } catch (Exception ex) {
+            LOGGER.error("An exception was thrown in validation service", ex);
+        }
+    }
+
+    private static boolean isCardNumberValid(final String cardNumber) {
+        try {
+            Long.parseLong(cardNumber);
+            return true;
+        } catch (NumberFormatException ex) {
+        }
+        return false;
+    }
+
+    private static void publishValidationEvent(final EventObject event) {
+
+        final ProducerRecord<Integer, EventObject> record =
+                new ProducerRecord<>("VALIDATION_EVENT_TOPIC", event.getCustomerId(), event);
+
+        try {
+            producer.beginTransaction();
+            final RecordMetadata metadata = producer.send(record).get();
+            producer.commitTransaction();
+            LOGGER.info("Validation event published to Kafka: Topic {} Partition {} Offset {}",
+                    metadata.topic(), metadata.partition(), metadata.offset());
+        } catch (InterruptedException e) {
+            LOGGER.error("An InterruptedException was thrown in publishValidationEvent method", e.getMessage());
+        } catch (ExecutionException e) {
+            LOGGER.error("An InterruptedException was thrown in publishValidationEvent method", e.getMessage());
+        } catch (ProducerFencedException e) {
+            LOGGER.error("An InterruptedException was thrown in publishValidationEvent method", e.getMessage());
+            producer.close();
+        } catch (KafkaException e) {
+            LOGGER.error("A KafkaException was thrown in publishValidationEvent method", e.getMessage());
+            producer.abortTransaction();
+        }
+    }
 }
---
+++ validation-service/src/main/java/jugistanbul/validationservice/kafka/producer/ValidationEventProducer.java
@@ -0,0 +1,41 @@
+package jugistanbul.validationservice.kafka.producer;
+
+import jugistanbul.entity.EventObject;
+import jugistanbul.serializer.CustomSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * @author hakdogan (hakdogan@kodcu.com)
+ * Created on 19.08.2020
+ **/
+
+public class ValidationEventProducer
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValidationEventProducer.class);
+
+    private ValidationEventProducer() {}
+
+    public static Producer<Integer, EventObject> build(){
+
+        LOGGER.info("Initialize validation event producer...");
+
+        final Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomSerializer.class.getName());
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString());
+
+        final Producer<Integer, EventObject> producer = new KafkaProducer<>(props);
+        producer.initTransactions();
+        return producer;
+    }
+}
